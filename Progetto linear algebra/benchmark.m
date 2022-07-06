093693b1f76f138ccfa6b5880d39c04935e7f8f9
daniel <scalena99@gmail.com>
2022-07-07 00:19:35 +0200

    Added matlab benchs

--- Progetto linear algebra/benchmark.m
+++ Progetto linear algebra/benchmark.m
@@ -1,12 +1,21 @@
 compute_cholesky("ex15.mat");
+disp('1')
 compute_cholesky("shallow_water1.mat")
+disp('2')
 compute_cholesky("parabolic_fem.mat")
+disp('3')
 compute_cholesky("apache2.mat")
+disp('4')
 compute_cholesky("G3_circuit.mat")
+disp('5')
 compute_cholesky("cfd1.mat")
+disp('6')
 compute_cholesky("cfd2.mat")
+disp('7')
 compute_cholesky("Flan_1565.mat")
+disp('8')
 compute_cholesky("StocF-1465.mat")
+disp('9')
 
 
 
@@ -47,6 +56,7 @@ end
 function [] = compute_cholesky(matrix_name)
     [relative_error] = compute_cholesky_relative_error(matrix_name);
     [read_times, solve_times] = cholesky_times(matrix_name);
+    disp([read_times, solve_times])
     writematrix(replace(string(cat(2, relative_error, cat(2, read_times, solve_times))), ".", ","), "results.txt", "WriteMode", "append", 'Delimiter','semi');
 end
 
