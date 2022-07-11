224ae0b9d28872eb3207b13eb90a962b2454e945
daniel <scalena99@gmail.com>
2022-07-11 16:03:59 +0200

    Now GUI working, start from main

---
+++ Progetto Fourier analysis/compressor.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+def main(path_image, F, d):
+    print(path_image)
+    print(F)
+    print(d)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
--- Progetto Fourier analysis/gui.py
+++ Progetto Fourier analysis/gui.py
@@ -3,18 +3,22 @@ import PySimpleGUI as sg
 import os.path
 from PIL import Image, ImageTk
 
+from compressor import main
+
+
+font = ("Arial", 13)
+
 
 # first the windown layout in 2 columns
-
 file_list_column = [
     [
-        sg.Text("Image Folder"),
+        sg.Text("Image Folder", font=font),
         sg.In(size = (25, 1), enable_events=True, key = '-FOLDER-', expand_x=True, expand_y=True),
         sg.FolderBrowse(),
     ],
     [
         sg.Listbox(
-            values = [], enable_events=True, size = (40, 20), key = "-FILE LIST-", expand_x=True, expand_y=True
+            values = [], enable_events=True, size = (50, 30), key = "-FILE LIST-", expand_x=True, expand_y=True
         ),
     ],
 ]
@@ -22,9 +26,19 @@ file_list_column = [
 
 # For now will only show the name of the file that was chosen
 image_viewer_column = [
-    [sg.Text("Choose an image from list on left:")],
+    [sg.Text("Choose an image from list on left:", font = font)],
     [sg.Text(size=(40, 1), key="-TOUT-")],
     [sg.Image(key="-IMAGE-", size = (30, 30), expand_x=True, expand_y=True)],
+]
+
+
+max_F = 300
+f_slider = [ 
+    [sg.Text("Scegli un intero F:", font = font)],
+    [sg.Slider(key = '-INTF-', range=(0, max_F), orientation='h', size=(70, 20), default_value = 100, enable_events = True)],
+    [sg.Text("Scegli un intero d compreso tra 0 e 2F - 2:", font = font)],
+    [sg.Slider(key = '-INTd-', range=(0, 2 * max_F - 2), orientation='h', size=(70, 20), default_value = 0, enable_events = True)],
+    [sg.Button('Start', size=(8, 1), key='-START_KEY-')]
 ]
 
 
@@ -34,10 +48,12 @@ layout = [
         sg.Column(file_list_column),
         sg.VSeparator(),
         sg.Column(image_viewer_column),
+        sg.VSeparator(),
+        sg.Column(f_slider),
     ]
 ]
 
-window = sg.Window("Image viewer", layout=layout, resizable=True, finalize=True)
+window = sg.Window("Image viewer", layout=layout, resizable=True, finalize=True, size=(1500, 600))
 
 # Evento loop:
 while True:
@@ -70,8 +86,8 @@ while True:
             )
             
             # Opening file
-            # Resize PNG file to size (300, 300)
-            size = (300, 300)
+            # Resize PNG file to size (500, 500)
+            size = (500, 500)
             im = Image.open(filename)
             im = im.resize(size, resample=Image.BICUBIC)
 
@@ -83,6 +99,23 @@ while True:
             window['-IMAGE-'].update(data=image)
         except:
             print(f'[X] Error while showing image {filename}')
+    elif event == '-INTF-':
+        try:
+            window['-INTd-'].update(range=(0, 2 * values['-INTF-'] - 2))
+        except:
+            print(f'[X] Error intF')
+    elif event == '-START_KEY-':
+        try:
+            if values['-FILE LIST-']:
+                filename = os.path.join(values['-FOLDER-'], values['-FILE LIST-'][0])
+                main(path_image = filename, F = values["-INTF-"], d = values["-INTd-"])
+            else:
+                print('Si, ma inserisci prima il file')
+                print(f'Woooo {values["-INTF-"]}, {values["-INTd-"]}, {values["-FOLDER-"]}, {values["-FILE LIST-"]}')
+        except Exception as e:
+            print(f'[X] Error while starting \n{e}')
+            print(f'Woooo {values["-INTF-"]}, {values["-INTd-"]}, {values["-FOLDER-"]}, {values["-FILE LIST-"]}')
+
 
 window.close()
 
